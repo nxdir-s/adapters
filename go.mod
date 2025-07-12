0ef4e10bde139110a3a1010d5e043f997232de41
nxdir-s <snadir177@gmail.com>
2025-07-11 21:57:12 -0400

    add postgres adapter

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.83.0
 	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.35.7
 	github.com/aws/smithy-go v1.22.4
+	github.com/jackc/pgx/v5 v5.7.5
 	github.com/pelletier/go-toml/v2 v2.2.4
 	github.com/twmb/franz-go v1.19.5
 	github.com/twmb/franz-go/pkg/kadm v1.16.0
@@ -36,6 +37,9 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/twmb/franz-go/pkg/kmsg v1.11.2 // indirect
@@ -44,6 +48,7 @@ require (
 	go.opentelemetry.io/otel/metric v1.37.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
---
+++ postgres.go
@@ -0,0 +1,285 @@
+package adapters
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type ErrConnect struct {
+	err error
+}
+
+func (e *ErrConnect) Error() string {
+	return "error creating connection pool: " + e.err.Error()
+}
+
+type ErrQueryRow struct {
+	err error
+}
+
+func (e *ErrQueryRow) Error() string {
+	return "error querying row: " + e.err.Error()
+}
+
+type ErrExecQuery struct {
+	err error
+}
+
+func (e *ErrExecQuery) Error() string {
+	return "error executing query: " + e.err.Error()
+}
+
+type ErrNotFound struct {
+	name string
+}
+
+func (e *ErrNotFound) Error() string {
+	return "error no rows affected"
+}
+
+type ErrCollectRows struct {
+	err error
+}
+
+func (e *ErrCollectRows) Error() string {
+	return "failed to collect rows to struct: " + e.err.Error()
+}
+
+type ErrCreateUser struct{}
+
+func (e *ErrCreateUser) Error() string {
+	return "failed to create new user"
+}
+
+type PgxPool interface {
+	Begin(ctx context.Context) (pgx.Tx, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error)
+}
+
+// NewPgxPool creates a pgxpool.Pool
+func NewPgxPool(ctx context.Context, dbUrl string) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(ctx, dbUrl)
+	if err != nil {
+		return nil, &ErrConnect{err}
+	}
+
+	return pool, nil
+}
+
+type PgxTx interface {
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+}
+
+type PostgresOpt[T any] func(a *PostgresAdapter[T])
+
+// WithPgxTx sets the transaction adapter
+func WithPgxTx[T any](tx PgxTx) PostgresOpt[T] {
+	return func(a *PostgresAdapter[T]) {
+		a.tx = tx
+	}
+}
+
+type PostgresRow struct {
+	Collection string
+	Query      string
+	NamedArgs  pgx.NamedArgs
+}
+
+type PostgresRows[T any] struct {
+	PostgresRow
+	Data []T
+}
+
+type PostgresAdapter[T any] struct {
+	conn   PgxPool
+	tx     PgxTx
+	logger *slog.Logger
+	tracer trace.Tracer
+}
+
+// NewPostgresAdapter creates a new PostgresAdapter
+func NewPostgresAdapter[T any](pool PgxPool, logger *slog.Logger, tracer trace.Tracer, opts ...PostgresOpt[T]) *PostgresAdapter[T] {
+	adapter := &PostgresAdapter[T]{
+		conn:   pool,
+		logger: logger,
+		tracer: tracer,
+	}
+
+	for _, opt := range opts {
+		opt(adapter)
+	}
+
+	return adapter
+}
+
+// NewTransactionAdapter creates an adapter for executing transactions
+func (a *PostgresAdapter[T]) NewTransactionAdapter(ctx context.Context) (*PostgresAdapter[T], error) {
+	tx, err := a.conn.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	txAdapter := NewPostgresAdapter[T](tx, a.logger, a.tracer, WithPgxTx[T](tx))
+
+	return txAdapter, nil
+}
+
+// Commit commits the transaction after first checking if the context has been canceled
+func (a *PostgresAdapter[T]) Commit(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+
+		return a.tx.Rollback(ctx)
+	default:
+		return a.tx.Commit(ctx)
+	}
+}
+
+// Rollback initiates a transaction rollback
+func (a *PostgresAdapter[T]) Rollback(ctx context.Context) error {
+	return a.tx.Rollback(ctx)
+}
+
+// Insert creates a new row returns the id
+func (a *PostgresAdapter[T]) Insert(ctx context.Context, row *PostgresRow) (int, error) {
+	ctx, span := a.tracer.Start(ctx, "INSERT "+row.Collection,
+		trace.WithLinks(trace.LinkFromContext(ctx)),
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(
+			attribute.String("db.system.name", "postgresql"),
+			attribute.String("db.operation.name", "INSERT"),
+			attribute.String("db.collection.name", row.Collection),
+			attribute.String("db.query.summary", "INSERT "+row.Collection),
+			attribute.String("db.query.text", row.Query),
+		),
+	)
+	defer span.End()
+
+	var id int
+	if err := a.conn.QueryRow(ctx, row.Query, row.NamedArgs).Scan(&id); err != nil {
+		return 0, &ErrExecQuery{err}
+	}
+
+	return id, nil
+}
+
+const InsertQuery string = `
+    INSERT INTO table (
+        data
+    ) VALUES (
+        @data
+    ) RETURNING id
+`
+
+// Delete deletes the supplied row
+func (a *PostgresAdapter[T]) Delete(ctx context.Context, row *PostgresRow) error {
+	ctx, span := a.tracer.Start(ctx, "DELETE "+row.Collection,
+		trace.WithLinks(trace.LinkFromContext(ctx)),
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(
+			attribute.String("db.system.name", "postgresql"),
+			attribute.String("db.operation.name", "DELETE"),
+			attribute.String("db.collection.name", row.Collection),
+			attribute.String("db.query.summary", "DELETE "+row.Collection),
+			attribute.String("db.query.text", row.Query),
+		),
+	)
+	defer span.End()
+
+	resp, err := a.conn.Exec(ctx, row.Query, row.NamedArgs)
+	if err != nil {
+		return &ErrExecQuery{err}
+	}
+
+	if resp.RowsAffected() == 0 {
+		return &ErrNotFound{}
+	}
+
+	return nil
+}
+
+const DeleteQuery string = `
+    DELETE FROM table
+    WHERE id = @id
+`
+
+func (a *PostgresAdapter[T]) Select(ctx context.Context, rows *PostgresRows[T]) error {
+	ctx, span := a.tracer.Start(ctx, "SELECT "+rows.Collection,
+		trace.WithLinks(trace.LinkFromContext(ctx)),
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(
+			attribute.String("db.system.name", "postgresql"),
+			attribute.String("db.operation.name", "SELECT"),
+			attribute.String("db.collection.name", rows.Collection),
+			attribute.String("db.query.summary", "SELECT "+rows.Collection),
+			attribute.String("db.query.text", rows.Query),
+		),
+	)
+	defer span.End()
+
+	output, err := a.conn.Query(ctx, rows.Query, rows.NamedArgs)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return &ErrQueryRow{err}
+	}
+
+	collection, err := pgx.CollectRows(output, pgx.RowToStructByName[T])
+	if err != nil {
+		return err
+	}
+
+	rows.Data = collection
+
+	return nil
+}
+
+const SelectQuery string = `
+    SELECT *
+    FROM table
+    WHERE id = @id
+`
+
+func (a *PostgresAdapter[T]) RowExists(ctx context.Context, row *PostgresRow) (bool, error) {
+	ctx, span := a.tracer.Start(ctx, "SELECT "+row.Collection,
+		trace.WithLinks(trace.LinkFromContext(ctx)),
+		trace.WithSpanKind(trace.SpanKindClient),
+		trace.WithAttributes(
+			attribute.String("db.system.name", "postgresql"),
+			attribute.String("db.operation.name", "SELECT"),
+			attribute.String("db.collection.name", row.Collection),
+			attribute.String("db.query.summary", "SELECT "+row.Collection+" by id"),
+			attribute.String("db.query.text", row.Query),
+		),
+	)
+	defer span.End()
+
+	var id int
+	err := a.conn.QueryRow(ctx, row.Query, row.NamedArgs).Scan(&id)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return false, &ErrQueryRow{err}
+	}
+
+	if id == 0 || errors.Is(err, pgx.ErrNoRows) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+const RowExistsQuery string = `
+    SELECT id
+    FROM table
+    WHERE id = @id
+`
